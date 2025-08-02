95e6d45afd4bff8339bc317e74194aae5044826c
Ruchit Rakholiya <ruchitrakholiya878@gmail.com>
2025-08-02 10:51:13 -0700

    useParams Error Solved

---
+++ app/social-auth-redirect/page.jsx
@@ -0,0 +1,9 @@
+// /app/social-auth-redirect/page.jsx
+'use client'
+export const dynamic = 'force-dynamic'
+
+import SocialAuthRedirectPage from '../../components/SocialAuthRedirectPage'
+
+export default function Page() {
+  return <SocialAuthRedirectPage />
+}
